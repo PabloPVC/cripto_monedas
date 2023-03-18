01ac8ea4204548170b8b2cd3ef3b0a9927d5dc9c
Pablo Rivadeneira <p_f_rivadeneira1@hotmail.com>
2023-03-18 18:26:55 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import '@/assets/css/tailwind.css'
+import Home from '@/views/Home'
+import Error from '@/views/Error'
+import About from '@/views/About'
+import CoinDetail from '@/views/CoinDetail'
+import { dollarFilter, percentFilter } from '@/filters'
+import Chart from 'chart.js'
+import Chartick from 'vue-chartkick'
+import { VueSpinners } from '@saeris/vue-spinners'
 
-Vue.config.productionTip = false;
+Vue.use(VueSpinners)
+Vue.use(Chartick.use(Chart))
+
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
+
+Vue.config.productionTip = false
+
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/about', name: 'about', component: About },
+  { path: '/coin/:id', name: 'coin-detail', component: CoinDetail },
+  { path: '*', name: 'error', component: Error },
+]
+
+const router = new VueRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  mode: 'history',
+  routes, // short for `routes: routes`
+})
+
+Vue.use(VueRouter)
 
 new Vue({
+  router,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app')
